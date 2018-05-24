cea675245e202de5b9d82fd20aae3d7c482d8a1c
Rowlandson Pratt <striderman34@gmail.com>
2018-05-24 15:34:53 +0100

    ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,52 +3,51 @@ extern crate rand;
 
 use ggez::conf;
 use ggez::event;
-use ggez::{Context,GameResult};
 use ggez::graphics;
+use ggez::{Context, GameResult};
 use std::env;
 use std::path;
 
-
 use rand::prelude::*;
 
-
 struct Walker {
-    x:f32,
-    y:f32,
+    x: f32,
+    y: f32,
 }
 
 impl Walker {
     pub fn new() -> Self {
-        Walker {
-            x:12.0,
-            y:12.0
-        }
+        Walker { x: 12.0, y: 12.0 }
     }
 
-    fn display(&self,ctx:&mut Context) -> GameResult<()> {
+    fn display(&self, ctx: &mut Context) -> GameResult<()> {
         graphics::set_color(ctx, [1.0, 0.0, 0.0, 1.0].into())?;
-        graphics::rectangle(ctx, graphics::DrawMode::Fill, graphics::Rect::new(self.x, self.y, 1.0, 1.0))?;
-        
+        graphics::rectangle(
+            ctx,
+            graphics::DrawMode::Fill,
+            graphics::Rect::new(self.x, self.y, 1.0, 1.0),
+        )?;
+
         Ok(())
     }
 
-    fn walk(&mut self,ctx:&mut Context) -> GameResult<()> {
+    fn walk(&mut self, ctx: &mut Context) -> GameResult<()> {
         let mut rng = thread_rng();
-        let choice:u16 = rng.gen_range(0,4);
+        let choice: u16 = rng.gen_range(0, 4);
 
         if choice == 0 {
             self.x += 1.0;
-        }else if choice == 1 {
+        } else if choice == 1 {
             self.x -= 1.0;
-        }else if choice == 2 {
+        } else if choice == 2 {
             self.y += 1.0;
-        }else {
+        } else {
             self.y -= 1.0;
         }
 
         Ok(())
     }
-    fn draw(&mut self,ctx:&mut Context)   -> GameResult<()> {
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         self.walk(ctx)?;
         self.display(ctx)?;
         Ok(())
@@ -56,14 +55,13 @@ impl Walker {
 }
 
 struct MainState {
-    walker:Walker
+    walker: Walker,
 }
 
-
 impl MainState {
-    fn new(ctx:&mut Context) -> GameResult<MainState> {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
         Ok(MainState {
-            walker:Walker::new()
+            walker: Walker::new(),
         })
     }
 }
@@ -73,7 +71,7 @@ impl event::EventHandler for MainState {
         Ok(())
     }
 
-    fn draw(&mut self,ctx:&mut Context) -> GameResult<()> {
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         self.walker.draw(ctx)?;
         graphics::present(ctx);
         Ok(())
