1f766956f6b82b9751e4779fce340858cb0a0a62
Lenard Pratt <striderman34@gmail.com>
2018-05-29 09:47:21 +0100

    work

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,46 @@ extern crate rand;
 use ggez::conf;
 use ggez::event;
 use ggez::graphics;
+use ggez::nalgebra as na;
 use ggez::{Context, GameResult};
+use std::cell::RefCell;
 
 use rand::prelude::*;
 
-struct Ball {
-    pos: graphics::Vector2,
-    vel: graphics::Vector2,
+thread_local!(static GENERATOR: RefCell<ThreadRng>= RefCell::new(thread_rng()));
+
+struct Star {
+    pos: na::Vector3<f32>,
 }
 
-impl Ball {
-    pub fn new() -> Self {
-        Ball {
-            pos: graphics::Vector2::new(100.0, 100.0),
-            vel: graphics::Vector2::new(2.0, 5.0),
+impl Star {
+    pub fn new(ctx: &mut Context) -> Self {
+        let (width, height) = graphics::get_window(ctx).size();
+
+        Star {
+            pos: na::Vector3::new(
+                GENERATOR.with(|cell| cell.borrow_mut().gen_range(-(width as f32), width as f32)),
+                GENERATOR.with(|cell| cell.borrow_mut().gen_range(-(width as f32), height as f32)),
+                width as f32,
+            ),
         }
     }
 
-    fn bounce(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.pos += self.vel;
-        if self.pos.x > 500.0 || self.pos.x < 0.0 {
-            self.vel.x = self.vel.x * -1.0;
-        }
-        if self.pos.y > 500.0 || self.pos.y < 0.0 {
-            self.vel.y = self.vel.y * -1.0;
-        }
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        self.pos.x += 3.0;
+        self.pos.y += 3.0;
+        self.pos.z += self.pos.z / 2.0;
+        // self.walker.walk(ctx)?;
         Ok(())
     }
 
     fn draw(&self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
         graphics::ellipse(
             ctx,
             graphics::DrawMode::Fill,
             graphics::Point2::new(self.pos.x, self.pos.y),
-            16.0,
-            16.0,
+            2.0,
+            2.0,
             1.0,
         )?;
         Ok(())
@@ -47,24 +51,48 @@ impl Ball {
 }
 
 struct MainState {
-    ball: Ball,
+    stars: Vec<Star>,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        Ok(MainState { ball: Ball::new() })
+        let mut stars = Vec::with_capacity(400);
+
+        for _ in 0..400 {
+            stars.push(Star::new(ctx));
+        }
+        Ok(MainState { stars })
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.ball.bounce(ctx)?;
-        // self.walker.walk(ctx)?;
+        for star in self.stars.iter_mut() {
+            star.update(ctx)?;
+        }
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.ball.draw(ctx)?;
+          graphics::clear(ctx);
+        let (width, height) = graphics::get_window(ctx).size();
+        let transform = graphics::Matrix4::new_rotation_wrt_point(
+            na::Vector3::new(width as f32, height as f32, width as f32),
+            na::Point3::new(
+                (width as f32) / 2.0,
+                (height as f32) / 2.0,
+                (width as f32) / 2.0,
+            ),
+        );
+        graphics::transform(ctx, transform);
+        graphics::apply_transformations(ctx)?;
+        graphics::set_background_color(ctx, graphics::Color::from_rgb(0, 0, 0));
+
+        for star in self.stars.iter() {
+            star.draw(ctx)?;
+        }
+
         graphics::present(ctx);
         Ok(())
     }
@@ -84,4 +112,8 @@ fn main() {
     }
 
     println!("Hello, world!");
+}
+
+fn map(value: f32, istart: f32, istop: f32, ostart: f32, ostop: f32) -> f32 {
+    ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
 }
